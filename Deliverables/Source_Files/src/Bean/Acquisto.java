5b681ffe6c3412f786da72d47e27b88a0d1961f6
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-04 11:20:37 +0100

    aggiunti i metodi get e set

--- Deliverables/Source_Files/src/Bean/Acquisto.java
+++ Deliverables/Source_Files/src/Bean/Acquisto.java
@@ -7,8 +7,41 @@ public class Acquisto {
 	private String prodotto;
 	
 	public Acquisto(){
-		
+		setCodice_acquisto(0);
+		setDescrizione("");
+		setFattura(0);
+		setProdotto("");
 	}
 
-	
+	public int getCodice_acquisto() {
+		return codice_acquisto;
+	}
+
+	public void setCodice_acquisto(int codice_acquisto) {
+		this.codice_acquisto = codice_acquisto;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public int getFattura() {
+		return fattura;
+	}
+
+	public void setFattura(int fattura) {
+		this.fattura = fattura;
+	}
+
+	public String getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(String prodotto) {
+		this.prodotto = prodotto;
+	}
 }
