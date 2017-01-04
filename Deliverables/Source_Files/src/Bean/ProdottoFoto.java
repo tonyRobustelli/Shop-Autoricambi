63733ff6ec1e99413a7376b46cb0706bca8437c0
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-04 13:25:44 +0100

    Aggiunto variabili di stato e metodi get e set alla classe PodottoFoto

--- Deliverables/Source_Files/src/Bean/ProdottoFoto.java
+++ Deliverables/Source_Files/src/Bean/ProdottoFoto.java
@@ -1,6 +1,31 @@
 package Bean;
 
 public class ProdottoFoto {
+    private Prodotto prodotto_bean;
+	private Foto foto_bean;
+
+ 
     public ProdottoFoto(){
+		setProdotto_bean(null);
+		setFoto_bean(null);
+	}
+
+    public Prodotto getProdotto_bean() {
+		return prodotto_bean;
+	}
+
+  
+    public void setProdotto_bean(Prodotto prodotto_bean) {
+		this.prodotto_bean = prodotto_bean;
+	}
+
+   
+    public Foto getFoto_bean() {
+		return foto_bean;
+	}
+
+
+    public void setFoto_bean(Foto foto_bean) {
+		this.foto_bean = foto_bean;
 	}
 }
