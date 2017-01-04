0ed0a646498e3de9b32500f1fce7d91200336ca7
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-04 13:27:34 +0100

    Aggiunti commenti javadoc alla classe PodottoFoto

--- Deliverables/Source_Files/src/Bean/ProdottoFoto.java
+++ Deliverables/Source_Files/src/Bean/ProdottoFoto.java
@@ -1,30 +1,53 @@
 package Bean;
 
+/**
+ *
+ * In base all'ogetto mi prendo l'imaggine appropriata, ci permette di realizzare 
+ * la relazione tra foto e prodotti
+ * 
+ * 
+ */
 public class ProdottoFoto {
-    private Prodotto prodotto_bean;
+
+	private Prodotto prodotto_bean;
 	private Foto foto_bean;
 
- 
+    /**
+     * Costruttore di ProdottoFoto, esso setta le variabili di stato
+     */
     public ProdottoFoto(){
 		setProdotto_bean(null);
 		setFoto_bean(null);
 	}
 
+    /**
+     * ritorna l'istanza di un oggetto prodotto 
+     * @return prodotto_bean
+     */
     public Prodotto getProdotto_bean() {
 		return prodotto_bean;
 	}
 
-  
+    /**
+     *  Setta l'oggetto prodotto
+     * @param prodotto_bean
+     */
     public void setProdotto_bean(Prodotto prodotto_bean) {
 		this.prodotto_bean = prodotto_bean;
 	}
 
-   
+    /**
+     * Ritorna un oggetto Foto 
+     * @return foto_bean
+     */
     public Foto getFoto_bean() {
 		return foto_bean;
 	}
 
-
+    /**
+     * Setta un oggetto Foto
+     * @param foto_bean
+     */
     public void setFoto_bean(Foto foto_bean) {
 		this.foto_bean = foto_bean;
 	}
