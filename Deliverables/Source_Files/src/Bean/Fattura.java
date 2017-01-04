71193a309bd7fad846e6640c3991414c15efb1b3
simonefinelli95 <simonfinelli@gmail.com>
2017-01-04 11:37:20 +0100

    Aggiunti i metodi get e set alla classe Fattura

--- Deliverables/Source_Files/src/Bean/Fattura.java
+++ Deliverables/Source_Files/src/Bean/Fattura.java
@@ -7,9 +7,53 @@ public class Fattura {
 	private float pagamento;
 	private String utente;
 	
+	
 	public Fattura(){
-		
+		setFattura(0);
+		setData(null);
+		setCosto_totale(0);
+		setPagamento(0);
+		setCliente("");
 		
 	}
 
+	public int getFattura() {
+		return this.CodiceFattura;
+	}
+
+	public void setFattura(int fattura) {
+		this.CodiceFattura = fattura;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public float getCosto_totale() {
+		return costototale;
+	}
+
+	public void setCosto_totale(float costototale) {
+		this.costototale = costototale;
+	}
+
+	public float getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(float pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public String getCliente() {
+		return utente;
+	}
+
+	public void setCliente(String utente) {
+		this.utente = utente;
+	}
 }
