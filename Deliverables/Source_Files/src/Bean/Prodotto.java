20b22cc72fd9bd88ee61ab83d2806a5ce62fef5a
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-04 11:39:26 +0100

    Aggiunto costruttore alla classe Prodotto

--- Deliverables/Source_Files/src/Bean/Prodotto.java
+++ Deliverables/Source_Files/src/Bean/Prodotto.java
@@ -1,7 +1,14 @@
 package Bean;
 
 public class Prodotto {
-	
+	private String cod_pezzo;
+	private String marchio;
+	private String modello;
+	private float prezzo;
+	private boolean offerta;
+	private float PrezzoScontato;
+	private String descrizione;
+	private int quantita;
 	
 	public Prodotto(){
 		
