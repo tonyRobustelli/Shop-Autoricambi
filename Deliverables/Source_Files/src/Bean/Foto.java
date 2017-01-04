375313bd641bcbe2d91d1b05bcd64582189c2dca
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-04 10:52:30 +0100

    Aggiunti i metodi get e set alla classe Foto

--- Deliverables/Source_Files/src/Bean/Foto.java
+++ Deliverables/Source_Files/src/Bean/Foto.java
@@ -1,9 +1,26 @@
 package Bean;
 
 public class Foto{
-	private String path;
+    private String path;
     private String prodotto;
     
-    public Foto(){        
+    
+    public Foto(){
+        setPath("");
+        setProdotto("");
+    }
+    
+    public String getProdotto() {
+        return prodotto;
+    }
+    
+    public void setProdotto(String prodotto) {
+        this.prodotto = prodotto;
+    }
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
     }
 }
