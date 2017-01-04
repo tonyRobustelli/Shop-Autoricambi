da48b83be94cb4dee98c6130f2da35c3ac379b6f
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-04 11:18:41 +0100

    aggiunto costruttore per la classe acquisto

--- Deliverables/Source_Files/src/Bean/Acquisto.java
+++ Deliverables/Source_Files/src/Bean/Acquisto.java
@@ -1,5 +1,14 @@
 package Bean;
 
 public class Acquisto {
+	private int codice_acquisto;
+	private String descrizione;
+	private int fattura;
+	private String prodotto;
+	
+	public Acquisto(){
+		
+	}
+
 	
 }
