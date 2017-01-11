4c177836b4c6fd1dcd7c56a893b4dc0f66ff88bf
tonyRobustelli <A.ROBUSTELLI29@studenti.unisa.it>
2017-01-11 14:53:22 +0100

    Aggiunte variabili di stato alla classe FotoAcquisto

--- Deliverables/Source_Files/src/storico/FotoAcquisto.java
+++ Deliverables/Source_Files/src/storico/FotoAcquisto.java
@@ -3,9 +3,13 @@ package storico;
 
 public class FotoAcquisto {
 	
+private String descrizione;
+private int num;
+private String path;
+private String cod_prodotto;
 
+   
     public FotoAcquisto(){
-
 	}
 
    
