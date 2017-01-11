a01548f3bb71ceeb99779ed5dc438aff1933205b
simonefinelli95 <simonfinelli@gmail.com>
2017-01-11 14:35:24 +0100

    Aggiunti commenti per documentazione Javadoc

--- Deliverables/Source_Files/src/Bean/Acquisto.java
+++ Deliverables/Source_Files/src/Bean/Acquisto.java
@@ -1,47 +1,89 @@
 package Bean;
 
+/**
+ * Rappresenta un entità 
+ * 
+ */
 public class Acquisto {
 	private int codice_acquisto;
 	private String descrizione;
 	private int fattura;
 	private String prodotto;
 	
-	public Acquisto(){
+    /**
+     * Costruttore, esso setta le variabili di stato
+     */
+    public Acquisto(){
 		setCodice_acquisto(0);
 		setDescrizione("");
 		setFattura(0);
 		setProdotto("");
 	}
 
-	public int getCodice_acquisto() {
+    /**
+     * ritorna l'identificativo di un acquisto 
+     * @return codice_acquisto 
+     */
+    public int getCodice_acquisto() {
 		return codice_acquisto;
 	}
 
-	public void setCodice_acquisto(int codice_acquisto) {
+    /**
+     * Setto l'identificativo di un acquisto 
+     * @param codice_acquisto
+     */
+    public void setCodice_acquisto(int codice_acquisto) {
 		this.codice_acquisto = codice_acquisto;
 	}
 
-	public String getDescrizione() {
+    /**
+     * ritorna la descrizione dell'acquisto
+     * @return descrizione
+     */
+    public String getDescrizione() {
 		return descrizione;
 	}
 
-	public void setDescrizione(String descrizione) {
+    /**
+     * Setta la descrizione dell'acquisto
+     * @param descrizione
+     * pre: descrizione diverso da null
+     */
+    public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
 
-	public int getFattura() {
+    /**
+     * Ritorna l'identificativo di una fattura
+     * @return fattura
+     */
+    public int getFattura() {
 		return fattura;
 	}
 
-	public void setFattura(int fattura) {
+    /**
+     * Setta l'identificativo di una fattura
+     * @param fattura
+     */
+    public void setFattura(int fattura) {
 		this.fattura = fattura;
 	}
 
-	public String getProdotto() {
+    /**
+     * Ritorna l'identificativo di un prodotto 
+     * @return prodotto
+     * post: prodoto!=null
+     */
+    public String getProdotto() {
 		return prodotto;
 	}
 
-	public void setProdotto(String prodotto) {
+    /**
+     * Setta l'identificati di un prodotto
+     * @param prodotto
+     * pre: prodotto!=null
+     */
+    public void setProdotto(String prodotto) {
 		this.prodotto = prodotto;
 	}
 }
