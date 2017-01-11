b419a838c91ca8052ab9cf1a05ab470346122f40
simonefinelli95 <simonfinelli@gmail.com>
2017-01-11 14:24:20 +0100

    Aggiunti i metodi get e set per la classe OggettoAmministratore

--- Deliverables/Source_Files/src/storico/OggettoAmministratore.java
+++ Deliverables/Source_Files/src/storico/OggettoAmministratore.java
@@ -1,10 +1,51 @@
 package storico;
 
-
 public class OggettoAmministratore {
+	
+private String descrizione;
+private int num;
+private String path;
+private String cod_prodotto;
 
-
+    /**
+     * Costruttore Vuoto
+     */
     public OggettoAmministratore(){
-		
+		this.setDescrizione("");
+		this.setNum(0);
+		this.setPath("");
+		this.setCod_prodotto("");
+	}
+
+    public String getDescrizione() {
+		return descrizione;
+	}
+
+    public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+    public int getNum() {
+		return num;
+	}
+
+    public void setNum(int num) {
+		this.num = num;
+	}
+
+    public String getPath() {
+		return path;
+	}
+
+    public void setPath(String path) {
+		this.path = path;
+	}
+
+    public String getCod_prodotto() {
+		return cod_prodotto;
+	}
+
+    public void setCod_prodotto(String cod_prodotto) {
+		this.cod_prodotto = cod_prodotto;
 	}
 }
