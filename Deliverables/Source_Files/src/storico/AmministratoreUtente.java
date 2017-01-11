abbaff8fc3c5e03f5b89649c73656077af029cef
tonyRobustelli <A.ROBUSTELLI29@studenti.unisa.it>
2017-01-11 14:43:11 +0100

    Aggiunte variabili di stato alla classe AmministratoreUtente

--- Deliverables/Source_Files/src/storico/AmministratoreUtente.java
+++ Deliverables/Source_Files/src/storico/AmministratoreUtente.java
@@ -2,10 +2,18 @@ package storico;
 
 
 public class AmministratoreUtente {
-
+	private String nome;
+	private String cognome;
+	private String num_fattura;
+	private String via;
+	private String cap;
+	private String data;
+	private ArrayList<FotoAcquisto> lista_ogg_amministratore;
+	
+    
     public AmministratoreUtente(){
-		
+
+	
 	}
-	
-	
+
 }
