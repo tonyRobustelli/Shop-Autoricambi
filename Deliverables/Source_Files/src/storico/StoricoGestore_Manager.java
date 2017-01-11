e4dc38b787a1ef1316bcc076eebbc1b0b632749a
simonefinelli95 <simonfinelli@gmail.com>
2017-01-11 13:38:44 +0100

    Definiti i metodi get e set per StoricoGestore

--- Deliverables/Source_Files/src/storico/StoricoGestore_Manager.java
+++ Deliverables/Source_Files/src/storico/StoricoGestore_Manager.java
@@ -1,11 +1,21 @@
 package storico;
 
-public class StoricoGestore_Manager {
+public class StoricoCliente_Manager {
     
     
-    public StoricoGestore_Manager(){
+    private ArrayList<AmministratoreUtente> lista_ogg_amministratore_cli;
+
+    public StoricoCliente_Manager(){
         
-        
-        
+        lista_ogg_amministratore_cli=new ArrayList<>();
     }
+    
+    public ArrayList<AmministratoreUtente> getLista_ogg_amministratore_cli() {
+        return lista_ogg_amministratore_cli;
+    }
+
+    public void setLista_ogg_amministratore_cli(ArrayList<AmministratoreUtente> lista_ogg_amministratore_cli) {
+        this.lista_ogg_amministratore_cli = lista_ogg_amministratore_cli;
+    }
+    
 }
