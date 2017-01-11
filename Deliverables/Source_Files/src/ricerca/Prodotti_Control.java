a028aaa48f205e5b89f61249da92491f2ee4a31d
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-11 13:13:18 +0100

    Estesa la Classe Prodotti_Control con HttpServlet

--- Deliverables/Source_Files/src/ricerca/Prodotti_Control.java
+++ Deliverables/Source_Files/src/ricerca/Prodotti_Control.java
@@ -1,4 +1,20 @@
+package ricerca;
 
-public class Prodotti_Control {
+public class Prodotti_Control extends HttpServlet {
+
+ 
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	
+	}
+	
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+			
+		
+	}
 
 }
