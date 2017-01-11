6bafc1fa8352c881047fa0aba38da4989153f2e1
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-11 13:39:54 +0100

    Implementati i metodi  della Classe ConnessioneDatabase

--- Deliverables/Source_Files/src/DBMS/ConnessioneDatabase.java
+++ Deliverables/Source_Files/src/DBMS/ConnessioneDatabase.java
@@ -1,11 +1,20 @@
 package DBMS;
 
+import java.sql.*;
+import java.lang.ClassNotFoundException;
+
 
 public class ConnessioneDatabase {
 
-  
-    public static Connection getConnection()  {
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
 	
-		return null;
-	}
+	Class.forName("com.mysql.jdbc.Driver");//carico il driver
+	
+	//stabilisco la connessione stabilendo username e password
+	Connection conn = (Connection) DriverManager.getConnection( "jdbc:mysql://localhost:3306/shop_autoricambi","root","gerardo" ) ;
+
+	
+	return conn;
+}
 }
