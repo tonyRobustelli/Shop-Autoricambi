fa511eea75607593a6f09116678670b860637fae
tonyRobustelli <A.ROBUSTELLI29@studenti.unisa.it>
2017-01-11 14:54:45 +0100

    Aggiunti i metodi set e get alla classe FotoAcquisto

--- Deliverables/Source_Files/src/storico/FotoAcquisto.java
+++ Deliverables/Source_Files/src/storico/FotoAcquisto.java
@@ -10,7 +10,46 @@ private String cod_prodotto;
 
    
     public FotoAcquisto(){
+		this.setDescrizione("");
+		this.setNum(0);
+		this.setPath("");
+		this.setCod_prodotto("");
+	}
+
+
+    public String getDescrizione() {
+		return descrizione;
+	}
+
+    public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+    public int getNum() {
+		return num;
 	}
 
    
+    public void setNum(int num) {
+		this.num = num;
+	}
+
+  
+    public String getPath() {
+		return path;
+	}
+
+
+    public void setPath(String path) {
+		this.path = path;
+	}
+
+
+    public String getCod_prodotto() {
+		return cod_prodotto;
+	}
+
+    public void setCod_prodotto(String cod_prodotto) {
+		this.cod_prodotto = cod_prodotto;
+	}
 }
