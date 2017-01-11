10867fa26bb5df8ceb24019aadefc0e1d04a0cf6
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-11 13:32:47 +0100

    Aggiunti metodi alla classe RicercaProdotto_Manager

--- Deliverables/Source_Files/src/ricerca/RicercaProdotto_Manager.java
+++ Deliverables/Source_Files/src/ricerca/RicercaProdotto_Manager.java
@@ -1,5 +1,49 @@
 package ricerca;
 
 
+
+
 public class RicercaProdotto_Manager {
+    
+    
+    //lista dei prodotti
+
+
+    public ArrayList<ProdottoFoto> lista_prodotti() throws ClassNotFoundException, SQLException{
+        
+        
+        return null;
+    }
+    
+    //lista dei prodotti carcati grazie alla servlet di ricerca
+
+    public ArrayList<ProdottoFoto> cerca_prodotti(Prodotto prodotto) {
+        
+        
+        return null;
+    }
+    
+    //filtra il prodotto da far vedere nella scheda tecnica
+
+    public Prodotto scheda_tecnica_prodotto(String cod_pezzo) {
+        
+        
+        
+        
+        return null;
+    }
+    
+    
+    //filtra le foto del prodotto
+
+   
+    public ArrayList<Foto> scheda_tecnica_foto(String cod_pezzo) {
+        
+        
+        return null;
+    }
+    
+   
+    
+    
 }
