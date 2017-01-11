e0db283114d6d88735184abf244b316f33fed2cb
giuseppesessa95 <giuseppesessa95@hotmail.it>
2017-01-11 12:44:21 +0100

    Estesa la Classe AmministratoreProdotti_Control con HttpServlet

--- Deliverables/Source_Files/src/caricamento/AmministratoreProdotti_Control.java
+++ Deliverables/Source_Files/src/caricamento/AmministratoreProdotti_Control.java
@@ -1,9 +1,18 @@
-
-public class AmministratoreProdotti_Control{
+public class AmministratoreProdotti_Control extends HttpServlet{
     
     public AmministratoreProdotti_Control() {
-       
+        super();
     }
     
+    
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+    {       
+    }
+    
+   
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+    {
+       
+    }
     
 }
