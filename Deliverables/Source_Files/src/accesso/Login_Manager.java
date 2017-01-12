06a86bdf606ee09dc6ef7e165054c3759d4355b0
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-12 13:01:12 +0100

    Implementati i metodi della classe Login_Manager.java

--- Deliverables/Source_Files/src/accesso/Login_Manager.java
+++ Deliverables/Source_Files/src/accesso/Login_Manager.java
@@ -1,13 +1,57 @@
 package accesso;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+import Bean.Utente;
+import DBMS.ConnessioneDatabase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 
 public class Login_Manager {
     
   
-    public Utente ControllaLogin(String e_m,String pass) 
+    public Utente ControllaLogin(String e_m,String pass) throws ClassNotFoundException, SQLException
     {
         
-       
+        //verifica se l'utente � presente nel DB
+        
+        Connection conn=(Connection) ConnessioneDatabase.getConnection();
+        Utente cliente=new Utente();
+        String query = "SELECT * FROM Utente WHERE email=? AND passw=?";
+        PreparedStatement ps=null;
+        ps=(PreparedStatement) conn.prepareStatement(query);
+        ps.setString(1, e_m);
+        ps.setString(2, pass);
+        ResultSet rs=ps.executeQuery();
+        if(!rs.next())
+            return(null);  ///Utente non trovato
+        else
+        {
+            rs=ps.executeQuery();
+            while (rs.next()) {
+                
+                cliente.setCodiceFiscale(rs.getString("cf"));
+                cliente.setCap(rs.getString("cap"));
+                cliente.setVia(rs.getString("via"));
+                cliente.setCitta(rs.getString("citta"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setDataNascita(rs.getString("data_nascita"));
+                cliente.setTelefono(rs.getString("telefono"));
+                cliente.setCellulare(rs.getString("cellulare"));
+                cliente.setEmail(rs.getString("email"));
+                cliente.setPassword(rs.getString("passw"));
+               
+                cliente.setAmministratore(rs.getBoolean("amministratore"));
+               
+                
+            }
+            System.out.println("vaivaivai"+cliente.getNome());
+            return(cliente);
+        }
         
         
     }
