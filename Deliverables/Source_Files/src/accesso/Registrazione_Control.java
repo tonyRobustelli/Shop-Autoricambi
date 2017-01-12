145760374e5c8b6b7f66f4fae042ba09818f001c
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-12 12:35:39 +0100

    Estesa la classe Registrazione_Control.java con HttpServlet

--- Deliverables/Source_Files/src/accesso/Registrazione_Control.java
+++ Deliverables/Source_Files/src/accesso/Registrazione_Control.java
@@ -1,12 +1,24 @@
 package accesso;
 
-public class Registrazione_Control {
-   
+public class Registrazione_Control extends HttpServlet {
+    
     public Registrazione_Control()
     {
+        super();
+    }
+
+    public void doGet(HttpServletRequest request,HttpServletResponse response) 
+    {
+       
+        
         
     }
+    
    
+    public void doPost(HttpServletRequest request,HttpServletResponse response) 
+    {
+
+    }
     
     
 }
