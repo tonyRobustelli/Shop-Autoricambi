d8c296b7121181fd60c6f2a2b53b58dfe536d9dc
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-12 12:43:18 +0100

    Aggiunti i metodi per Insert e il controllo dell'utente alla classe Registrazione_Manager.java

--- Deliverables/Source_Files/src/accesso/Registrazione_Manager.java
+++ Deliverables/Source_Files/src/accesso/Registrazione_Manager.java
@@ -2,5 +2,16 @@ package accesso;
 
 public class Registrazione_Manager  {
     
+
+    public void SalvaCliente(Utente utente) throws ClassNotFoundException, SQLException
+    {
+        
+    }
+    
  
+
+    public boolean ControllaRegistrazioneUtente(String codice_fiscale) throws ClassNotFoundException, SQLException
+    {
+        
+    }
 }
