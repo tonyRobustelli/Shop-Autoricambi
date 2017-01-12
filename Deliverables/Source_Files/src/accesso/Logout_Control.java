03ad5c185416823f9a54ed30d1528296130443dd
tonyRobustelli <A.ROBUSTELLI29@studenti.unisa.it>
2017-01-12 22:08:56 +0100

    Estesa la classe Logout_Control.java

--- Deliverables/Source_Files/src/accesso/Logout_Control.java
+++ Deliverables/Source_Files/src/accesso/Logout_Control.java
@@ -1,9 +1,23 @@
 package accesso;
 
-public class Logout_Control {
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Logout_Control extends HttpServlet{
 
     public Logout_Control() {
        super();
 	 }
-
+	
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		
+	}
+	
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+	  doGet(request,response);
+	}  
 }
