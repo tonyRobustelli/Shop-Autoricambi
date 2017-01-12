8ba2bfb32639d93f52c330b4f53ed5d0521a944c
gerardodellamonica <gerardo.dellamonica95@gmail.com>
2017-01-12 12:50:08 +0100

    Estesa la classe Login_Control.java con HttpServlet

--- Deliverables/Source_Files/src/accesso/Login_Control.java
+++ Deliverables/Source_Files/src/accesso/Login_Control.java
@@ -2,13 +2,23 @@ package accesso;
 
 
 
-public class Login_Control {
+public class Login_Control extends HttpServlet{
     
   
     public Login_Control()
     {
        
     }
-
+    
+   
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+    {
+    }
+    
+   
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException
+    {
+       
+    }
     
 }
